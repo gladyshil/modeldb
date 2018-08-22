2a29a73fe449e64f3bd1d88b6d61ee1c699c9f19
Gladys Hilerio <gladys_hilerio@msn.com>
2018-08-22 16:02:00 -0700

    Removed Thrift ARG and changed .dockerignore

--- dockerbuild/wait_for_mongodb.sh
+++ dockerbuild/wait_for_mongodb.sh
@@ -14,12 +14,11 @@ until mongo "$host"/foo --eval "db.getCollectionNames()" > /dev/null 2>&1; do
     >&2 echo "MongoDB is unavailable - sleeping"
     sleep 1
 done
+
 >&2 echo "MongoDB is up - executing command"
 
-
 # Substitute the desired MongoDB hostname in the built project's conf file.
-before="$PWD"
 sed -i "s/MONGODB_HOST/$host/" /modeldb/reference.conf
-cd "$before"
+
 
 java -Dconfig.file=/modeldb/reference.conf  -jar /modeldb/modeldb.jar
